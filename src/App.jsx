12b41c5ad0aeb2cacc7898f9ed23f850734be777
min3eo <110177217+min3eo@users.noreply.github.com>
2024-10-26 17:56:44 +0900

    Feat: 기본 css 구현하기

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,15 @@ import New from "./pages/new/New";
 import Diary from "./pages/diary/Diary";
 import NotFound from "./pages/NotFound";
 
+//이미지 가져오기 유틸 훅
+import getEmotionImage from "./utils/get-emotion-images";
+
 function App() {
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} alt='이미지'/>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
